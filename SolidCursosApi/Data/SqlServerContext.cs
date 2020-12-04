010c5b6d9347d2ba009057970aad9240d4d6f55d
Rafael Vissontai de Oliveira <rafael.vissontai@outlook.com>
2020-12-04 18:22:53 -0300

    Add project files.

---
+++ SolidCursosApi/Data/SqlServerContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolidCursosApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolidCursosApi.Data
+{
+    public class SqlServerContext : DbContext
+    {
+        public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Password=123;Persist Security Info=True;User ID=sa;Initial Catalog=SolidCursos;Data Source=DESKTOP-8NJ5G79\SQLEXPRESS");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
